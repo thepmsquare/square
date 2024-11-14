9addcd773138af42760097d1e01f60c62a7182f7
Parth Mangtani <impmmmm@gmail.com>
2024-11-14 20:50:05 +0530

    🌱 update landing page links

--- config/serverLinks.ts
+++ config/serverLinks.ts
@@ -2,23 +2,27 @@ import type { ServerLinks } from "@/types/ServerLinks";
 import { v4 as uuid } from "uuid";
 
 const serverLinks: ServerLinks = [
-  { id: uuid(), component: "server landing page", link: "https://localhost/" },
-  { id: uuid(), component: "pgadmin", link: "http://localhost:10101/" },
   {
     id: uuid(),
-    component: "square_database",
-    link: "https://localhost:10010/docs/",
+    component: "server landing page",
+    link: "https://raspi.thepmsquare.com/",
   },
-  {
-    id: uuid(),
-    component: "square_authentication",
-    link: "https://localhost:10011/docs/",
-  },
-  {
-    id: uuid(),
-    component: "square_file_store",
-    link: "https://localhost:10100/docs/",
-  },
+  // { id: uuid(), component: "pgadmin", link: "http://localhost:10101/" },
+  // {
+  //   id: uuid(),
+  //   component: "square_database",
+  //   link: "https://localhost:10010/docs/",
+  // },
+  // {
+  //   id: uuid(),
+  //   component: "square_authentication",
+  //   link: "https://localhost:10011/docs/",
+  // },
+  // {
+  //   id: uuid(),
+  //   component: "square_file_store",
+  //   link: "https://localhost:10100/docs/",
+  // },
 ];
 
 export default serverLinks;
